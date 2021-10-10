625963533e1e32c0540705cb940e28b54b595999
celine.hy.chiu <a4521005@gmail.com>
2021-10-11 00:35:11 +0800

    add DefaultLayout and Header

---
+++ src/components/layout/DefaultLayout.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled ,{css}from 'styled-components';
+import Header from './Header';
+
+const PageHeader = styled.div`
+    ${(props) => 
+    props.fixed &&
+    css`
+        position: fixed;
+        `}
+`;
+
+const DefaultLayout = ({fixedHeader, children}) =>{
+    return (
+    <div>
+        <PageHeader fixed={fixedHeader}>
+            <Header/>
+        </PageHeader>
+        {children}
+        <footer>This is footer</footer>
+    </div>
+    );
+}
+
+
+export default DefaultLayout;
---
+++ src/components/layout/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+
+const StyledHeader = styled.header`
+    background-color: #d1011c;
+    height:200px;
+    width: 100vw;
+`
+const Header = () =>{
+    return <StyledHeader>This is a header</StyledHeader>
+};
+
+export default Header;
