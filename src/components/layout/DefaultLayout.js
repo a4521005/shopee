ca19326701a3465b2595bb166886ca13de7ab02b
celine.hy.chiu <a4521005@gmail.com>
2021-10-16 14:31:13 +0800

    fixed position fixed

--- src/components/layout/DefaultLayout.js
+++ src/components/layout/DefaultLayout.js
@@ -4,18 +4,21 @@ import Header from './Header';
 import Footer from './Footer';
 import Container from '../common/Container';
 
-const PageHeader = styled(Header)`
+const PageHeader = styled.div`
     ${(props) => 
     props.fixed &&
     css`
         position: fixed;
+        z-index:1;
         `}
 `;
 
 const DefaultLayout = ({fixedHeader, children}) =>{
     return (
     <div>
-        <PageHeader fixed={fixedHeader} />
+        <PageHeader fixed={fixedHeader} >
+            <Header />
+        </PageHeader>
         <Container>{children}</Container>
         <Footer />
     </div>
